1d6bb423d5f371e5a89894067917ea2b57c56719
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2021-03-17 18:24:48 +0200

    Create menu component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+import Header from './components/Header';
+import Menu from './components/Menu';
+
 import style from './App.module.css';
-import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className={style.app}>
         <Header />
+        <Menu />
     </div>
   );
 }
